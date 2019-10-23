a8bb0e0ba5e73e30810fa0fa23b26e263d40f2c8
eldenhowes <williameldenhowes@gmail.com>
2019-10-23 09:24:38 +0100

    Refactored to use the fighters array

--- streetFighterSelection.js
+++ streetFighterSelection.js
@@ -1,9 +1,11 @@
 const streetFighterSelection = (fighters, position, moves) => {
+  let result = [];
   if (moves === undefined || moves.length == 0) {
     return moves;
   }
   if (moves[0] === "right") {
-    return ["E.Honda"];
+    result.push(fighters[0][1]);
   }
+  return result;
 };
 module.exports = streetFighterSelection;
